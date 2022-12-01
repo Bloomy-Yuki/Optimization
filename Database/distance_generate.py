b350fe33b9812a53439777feed6dec5188bf8d08
Sophie altayf <sophie.altayf@gmail.com>
2022-12-01 23:17:52 +0100

    added database

--- Database/distance_generate.py
+++ Database/distance_generate.py
@@ -1,15 +1,18 @@
-import numpy
+from numpy import round
 from math import *
-upper_limit = int(input("Enter the upper limit:\n"))
-distance_data = open('distance.txt','a')
-j = 0
-string_line = ''
-for i in range(1,upper_limit):
-    j = 0
-    string_line = ''
-    while j <= i :
-        distance_data.write(' ' + str(numpy.round(sqrt(int(i)**2 + j**2),3)))
-        j += 1
-    distance_data.write('\n')
-
-distance_data.close()
+def write_data_set(upper_limit):
+    #Setting up the upper limit of the data
+    #Opening up the file once again to fill it up with potentially new information
+    distance_data = open('distance.txt','a')
+    row = 0
+    #Filling out information anout the computation
+    for col in range(0,upper_limit):
+        distance_data.write(str(int(col)))
+        row = 1
+        while row <= upper_limit - 1:
+            #What to fill in every line is expressed here in the expression
+            distance_data.write(', '  + str(round(sqrt(int(col)**2 + row**2),3)))
+            row += 1
+        distance_data.write('\n')
+    distance_data.close()
+    return print("Done \n")
